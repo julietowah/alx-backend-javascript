dde107d24d5b13a38bb300a0b1d4952ffa14ea1c
Julietowah <julietowah@gmail.com>
2023-03-28 14:48:37 +0100

    correction

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom { // just add "default"
   constructor(maxStudentSize) {
-    this.maxStudentSize = maxStudentSize;
+    this._maxStudentSize = maxStudentSize;
   }
 }
